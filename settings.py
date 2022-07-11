bfe683ed2fc26c12a805b22ff9a56ddf5dee51b5
mikeGugs <michaelrguglielmo@gmail.com>
2022-07-11 06:01:11 -0400

    bunch of new stuff

---
+++ settings.py
@@ -0,0 +1,16 @@
+class Settings:
+    """A class to store all settings for Alein Invasion."""
+
+    def __init__(self):
+        """Initialize the game's settings"""
+        # Screen settings
+        self.screen_width = 0
+        self.screen_height = 0
+        self.bg_color = (230, 230, 230)
+        self.ship_speed = 1.5
+        # Bullet settings
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 3
